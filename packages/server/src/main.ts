f4be706b381f6d3beb68cc36150e73c450ba82fe
Caleb Carithers <calebcarithers@me.com>
2022-11-27 18:35:05 -0600

    change port

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = app.get(ConfigService).get('port');
+  Logger.log(`api server listeneing on port: ${port}`);
+  await app.listen(port);
 }
 bootstrap();
