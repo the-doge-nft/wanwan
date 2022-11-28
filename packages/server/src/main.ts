f35d80f4c71c77830ca6c032f14c394238baf95c
Caleb Carithers <calebcarithers@me.com>
2022-11-27 18:40:27 -0600

    commit local env vars

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = app.get(ConfigService).get('port');
-  Logger.log(`api server listeneing on port: ${port}`);
+  Logger.log(`listeneing on port: ${port}`);
   await app.listen(port);
 }
 bootstrap();
