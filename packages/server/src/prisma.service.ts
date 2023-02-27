dd4873d965864d9b1b2e280acb2d7e599ce178f1
Caleb Carithers <calebcarithers@me.com>
2023-02-27 13:03:10 -0600

    up

--- packages/server/src/prisma.service.ts
+++ packages/server/src/prisma.service.ts
@@ -1,10 +1,22 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
   async onModuleInit() {
-    await this.$connect();
+    await this.$connect()
+      .then(() => this.logger.log('Connected to database'))
+      .catch((e) => {
+        this.logger.error('Could not connect to database');
+        this.logger.error(e);
+      });
   }
 
   async enableShutdownHooks(app: INestApplication) {
