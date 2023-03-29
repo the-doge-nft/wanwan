d7a08ebb8f030b729bd958bef5573b0c57a3e744
Caleb Carithers <calebcarithers@me.com>
2023-03-29 11:51:13 -0500

    another admin

--- packages/server/src/auth/admin.guard.ts
+++ packages/server/src/auth/admin.guard.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from './auth.guard';
 export const ADMIN_ADDRESSES = [
   '0xd801d86C10e2185a8FCBccFB7D7baF0A6C5B6BD5',
   '0x18F33CEf45817C428d98C4E188A770191fDD4B79',
+  '0x933B2863f079F2E7033cfE0BadcfEa3378455F17',
 ];
 
 @Injectable()
