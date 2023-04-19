d9359740ee59d67f5e02404104ba1765232db56b
Caleb Carithers <calebcarithers@me.com>
2023-04-19 11:29:08 -0500

    drop ws provider

--- packages/server/src/ethers/ethers.service.ts
+++ packages/server/src/ethers/ethers.service.ts
@@ -1,17 +1,16 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { InjectSentry, SentryService } from '@travelerdev/nestjs-sentry';
 import { ethers } from 'ethers';
 import { AppEnv, Config } from '../config/config';
 import { CacheService } from './../cache/cache.service';
 
 @Injectable()
-export class EthersService implements OnModuleInit {
+export class EthersService {
   private readonly logger = new Logger(EthersService.name);
   private secondsToCacheEns = 60 * 60 * 10;
 
   public network: string;
-  public provider: ethers.providers.WebSocketProvider;
+  public provider: ethers.providers.AlchemyProvider;
   public zeroAddress = ethers.constants.AddressZero;
 
   private getEnsCacheKey(address: string) {
@@ -21,7 +20,6 @@ export class EthersService implements OnModuleInit {
   constructor(
     private readonly config: ConfigService<Config>,
     private readonly cache: CacheService,
-    @InjectSentry() private readonly sentryClient: SentryService,
   ) {
     const appEnv = this.config.get('appEnv');
     if (appEnv === AppEnv.production) {
@@ -34,89 +32,10 @@ export class EthersService implements OnModuleInit {
       this.logger.error('App environment unknown');
       throw new Error('App environment unknown');
     }
-  }
-
-  async onModuleInit() {
-    try {
-      this.logger.log('ethers service init');
-      this.initWS();
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  initWS() {
-    const logMessage = `Creating WS provider on network: ${
-      this.network
-    }:${this.config.get('appEnv')}`;
-    this.logger.error(logMessage);
-    this.sentryClient.instance().captureMessage(logMessage);
-
-    if (this.config.get('appEnv') === AppEnv.test) {
-      this.provider = new ethers.providers.WebSocketProvider(
-        `ws://127.0.0.1:8545`,
-      );
-    } else {
-      this.provider = new ethers.providers.WebSocketProvider(
-        this.config.get('alchemy').wsEndpoint,
-        this.network,
-      );
-    }
-    // this.eventEmitter.emit(Events.ETHERS_WS_PROVIDER_CONNECTED, this.provider);
-
-    if (this.config.get('appEnv') !== AppEnv.test) {
-      this.keepAlive({
-        provider: this.provider,
-        onDisconnect: () => {
-          this.initWS();
-        },
-      });
-    }
-  }
-
-  // ws connection will randomly drop, this makes sure we are always connected
-  keepAlive({
-    provider,
-    onDisconnect,
-    expectedPongBack = 15000,
-    checkInterval = 7500,
-  }: any) {
-    let pingTimeout: any;
-    let keepAliveInterval: any;
-
-    provider._websocket.on('open', () => {
-      const logMessage = 'Ethers provider ws connection open';
-      this.logger.log(logMessage);
-      keepAliveInterval = setInterval(() => {
-        provider._websocket.ping();
-
-        pingTimeout = setTimeout(() => {
-          provider._websocket.terminate();
-        }, expectedPongBack);
-      }, checkInterval);
-    });
-
-    provider._websocket.on('close', (err) => {
-      const logMessage = 'Ethers provider ws connection closed';
-      this.logger.error(logMessage);
-      this.sentryClient.instance().captureMessage(logMessage);
-
-      if (keepAliveInterval) {
-        clearInterval(keepAliveInterval);
-      }
-
-      if (pingTimeout) {
-        clearTimeout(pingTimeout);
-      }
-
-      onDisconnect(err);
-    });
-
-    provider._websocket.on('pong', () => {
-      if (pingTimeout) {
-        clearTimeout(pingTimeout);
-      }
-    });
+    this.provider = new ethers.providers.AlchemyProvider(
+      this.network,
+      this.config.get('alchemy').apiKey,
+    );
   }
 
   async refreshEnsCache(address: string) {
