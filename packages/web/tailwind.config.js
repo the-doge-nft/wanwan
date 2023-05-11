53834368fed7c53479484b5275dd71c034cec198
Caleb Carithers <calebcarithers@me.com>
2023-05-11 11:40:13 +0900

    hover only if supported

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
